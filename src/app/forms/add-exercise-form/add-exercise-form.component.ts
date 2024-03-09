7dc2fabfe4a34559f9d309e08b305235757a3e62
CristóbalJJG <cristobal_jjg@hotmail.com>
2024-03-09 11:10:15 +0000

    Creacion de formulario para Entrenamientos

---
+++ src/app/forms/add-exercise-form/add-exercise-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-exercise-form',
+  templateUrl: './add-exercise-form.component.html',
+  styleUrls: ['./add-exercise-form.component.scss'],
+})
+export class AddExerciseFormComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
